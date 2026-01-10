01d818f5bfcea1ec9353fd9a3784f8c0c50cdda7
Rhane Corporation <rhanecorp@gmail.com>
2026-01-10 17:06:31 +0530

    add email verification using OTP with auth middleware

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,6 +2,7 @@ import userModel from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import generateTokenAndSetCookie from "../utils/generateTokenAndSetCookie.js";
 import sendEmail from "../config/nodemailer.js";
+import generateOtp from "../utils/generateOtp.js";
 
 export const signup = async (req, res) => {
   const { name, email, password } = req.body;
@@ -86,6 +87,87 @@ export const login = async (req, res) => {
     return res.json({
       success: true,
       user: { user },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const sendVerificationOtp = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const user = await userModel.findOne({ _id: userId });
+
+    if (user.isAccountVerified) {
+      return res.json({
+        success: false,
+        message: "Account already verified.",
+      });
+    }
+
+    const otp = generateOtp();
+    user.verificationOtp = otp;
+    user.verificationOtpExpireAt = Date.now() + 1 * 60 * 60 * 1000;
+
+    await user.save();
+
+    const email = user.email;
+    const subject = "🗝️ Account Verification OTP";
+    const message = `Your OTP is ${otp}. Verify your account using this OTP.`;
+
+    sendEmail(email, subject, message);
+
+    return res.status(200).json({
+      success: true,
+      message: "Verification OTP sent on your email.",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const verifyAccount = async (req, res) => {
+  const userId = req.user.id;
+  const { otp } = req.body;
+  try {
+    const user = await userModel.findOne({ _id: userId });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "🔍 User not found.",
+      });
+    }
+
+    if (user.verificationOtp === "" || user.verificationOtp !== otp) {
+      return res.status(400).json({
+        success: false,
+        message: "⚠️ Invalid OTP. Please try again.",
+      });
+    }
+
+    if (user.verificationOtpExpireAt < Date.now()) {
+      return res.status(410).json({
+        success: false,
+        message: "⚠️ OTP Expired.",
+      });
+    }
+
+    user.isAccountVerified = true;
+    user.verificationOtp = "";
+    user.verificationOtpExpireAt = "";
+
+    await user.save();
+
+    return res.status(200).json({
+      success: false,
+      message: "✅ Email verified successfully.",
     });
   } catch (error) {
     return res.status(500).json({
--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { login, signup } from "../controllers/authController.js";
+import {
+  login,
+  sendVerificationOtp,
+  signup,
+  verifyAccount,
+} from "../controllers/authController.js";
+import userAuth from "../middleware/userAuth.js";
 
 const authRouter = express.Router();
 
 authRouter.route("/signup").post(signup);
 authRouter.route("/login").post(login);
+authRouter.route("/send-verification-otp").post(userAuth, sendVerificationOtp);
+authRouter.route("/verify-account").post(userAuth, verifyAccount);
 
 export default authRouter;
