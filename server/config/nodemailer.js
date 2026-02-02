ca703283e335ce872de93dc546d9bc20cb197a4f
Rhane Corporation <rhanecorp@gmail.com>
2026-02-02 05:34:59 +0530

    add service gmail

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,8 +1,7 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
+  service: "gmail",
   secure: true, // Use true for port 465, false for port 587
   auth: {
     user: "adithyarhane@gmail.com",
